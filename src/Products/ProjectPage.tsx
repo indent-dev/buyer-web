ce3f822d9f7349ddf9a41ba10b1035021ad850c6
Naufal Yukafi <naufalyukafi10@gmail.com>
2020-12-12 09:03:25 +0700

    test

--- src/Products/ProjectPage.tsx
+++ src/Products/ProjectPage.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Layout, Typography, Row, Col} from "antd";
+import { Layout, Typography, Row, Col } from "antd";
 const ProjectPage = () => {
     return (
-        <Layout style={{backgroundColor: "#fff"}}>
-            <Layout.Header style={{ backgroundColor: "#f6f7f7"}}>
+        <Layout style={{ backgroundColor: "#fff" }}>
+            <Layout.Header style={{ backgroundColor: "#f6f7f7" }}>
                 <Row align="middle" justify="space-between">
                     <Col>
                         <Typography.Title level={4}>Buyer Website</Typography.Title>
@@ -13,12 +13,13 @@ const ProjectPage = () => {
                     </Col>
                 </Row>
             </Layout.Header>
-                <Layout.Content style={{height: "90vh"}}>
-                    <Typography.Title level={4} style={{textAlign: "center"}} >Project Page</Typography.Title>
-                </Layout.Content>
+            <Layout.Content style={{ height: "90vh" }}>
+                <Typography.Title level={4} style={{ textAlign: "center" }} >Project Page</Typography.Title>
+                <Typography.Title level={4} style={{ textAlign: "center" }} >Teamss: Yukafi & Ilham</Typography.Title>
+            </Layout.Content>
             <Layout.Footer style={{ textAlign: 'center' }}>Footer</Layout.Footer>
         </Layout>
-      );
+    );
 }
 
 export default ProjectPage
